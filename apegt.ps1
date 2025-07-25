39d141ec061c9db176c01b03607db25af92f2de6
gavin-handley <gavin.handley@gmail.com>
2025-07-25 14:58:06 +1000

    Update apegt.ps1
    Added  -SkipPublisherCheck to the Get-WindowsAutopilotInfo line to speed up the process

--- apegt.ps1
+++ apegt.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 $groupTag = Read-Host "Enter Group Tag for Autopilot enrollment"
-Install-Script -Name Get-WindowsAutopilotInfo -Force
+Install-Script -Name Get-WindowsAutopilotInfo -Force -SkipPublisherCheck
 Get-WindowsAutopilotInfo -Online -GroupTag $groupTag
